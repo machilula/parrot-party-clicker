37d0b0d49b24acd8e5fa2baf5eb078b044c0c1ee
Lula <mlusan95@gmail.com>
2024-11-22 15:18:08 -0500

    part 1 in progress

--- src/index.js
+++ src/index.js
@@ -13,3 +13,34 @@
 // Box 2: When the spacebar is pressed down, make the text in Box 2 turn into a flan emoji
 // Box 3: When Box 3 is clicked on, make Party Parrot appear and disappear
 // Box 4: When Box 4 is double clicked, an alert shows up
+
+const state = {
+  parrotClick: null,
+  clickCount: 0
+}
+
+// const increaseCount = () => {
+//   let click = 0; 
+//   while (click < 10) {
+//     click++
+//   }
+// };
+
+const loadControls = () => {
+  state.parrotClick = document.getElementById('parrot-click');
+  state.clickCount = document.getElementById('click-count');
+
+}
+
+const clickParrotListener = () => {
+  ++state.clickCount;
+  state.clickCount.textContent = state.clickCount;
+}
+
+const registerEventHandlers = () => {
+  loadControls();
+  state.parrotClick.addEventListener('click', () => {
+  });
+};
+
+document.addEventListener('DOMContentLoaded', registerEventHandlers);
